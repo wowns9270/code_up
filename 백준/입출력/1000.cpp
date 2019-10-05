91d85faba2ff8b7a1db36ed801e0555ba63bdccf
wowns9270 <46587806+wowns9270@users.noreply.github.com>
2019-10-06 00:51:31 +0900

    Create 1000.cpp

---
+++ 백준/입출력/1000.cpp
@@ -0,0 +1,12 @@
+#include<cstdio>
+#pragma warning(disable:4996)
+using namespace std;
+
+
+
+// test ÇÕ´Ï´Ù~~
+int main() {
+	int a, b; scanf("%d %d", &a, &b);
+	printf("%d", a + b);
+	return 0;
+}
