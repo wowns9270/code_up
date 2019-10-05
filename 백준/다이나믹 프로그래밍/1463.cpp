1b1e5fce9b92c509f2c1782f33df196b3180c821
wowns9270 <46587806+wowns9270@users.noreply.github.com>
2019-10-06 01:04:29 +0900

    Update 1463.cpp

--- 백준/다이나믹 프로그래밍/1463.cpp
+++ 백준/다이나믹 프로그래밍/1463.cpp
@@ -15,10 +15,10 @@ int main() {
 		dp[i] = dp[i - 1] + 1;
 
 		if(i%2==0)
-		dp[i] = max(dp[i], dp[i / 2] + 1);
+		dp[i] = min(dp[i], dp[i / 2] + 1);
 
 		if (i % 3 == 0)
-		dp[i] = max(dp[i], dp[i / 3] + 1);
+		dp[i] = min(dp[i], dp[i / 3] + 1);
 	}
 	printf("%d", dp[n]);
 	return 0;
