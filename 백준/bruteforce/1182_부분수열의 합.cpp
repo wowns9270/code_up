56de6f0dcd1ddcad827e6929e2914a06ff5026ac
wowns9270 <46587806+wowns9270@users.noreply.github.com>
2019-12-24 10:45:07 +0900

    Create 1182_부분수열의 합.cpp

---
+++ 백준/bruteforce/1182_부분수열의 합.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int>rotto;
+vector<int>result;
+int n;
+
+void bruteforce(int cnt, int idx) {
+	if (cnt == 6) {
+
+		for (int i = 0; i < result.size(); i++) {
+			cout << result[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	if (idx == n) return;
+
+	result.push_back(rotto[idx]);
+	bruteforce(cnt + 1, idx + 1);
+	result.pop_back();
+
+	bruteforce(cnt, idx + 1);
+	return;
+}
+
+int main() {
+
+	while (1) {
+		rotto.clear();
+		cin >> n;
+		if (n == 0) break;
+
+		for (int i = 0; i < n; i++) {
+			int k; cin >> k;
+			rotto.push_back(k);
+		}
+
+		bruteforce(0, 0);
+		cout << "\n";
+	}
+	return 0;
+}
