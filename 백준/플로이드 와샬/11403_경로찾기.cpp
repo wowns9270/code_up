89fadebc33b9f58894d8526d4a86e4a50605c5d7
wowns9270 <46587806+wowns9270@users.noreply.github.com>
2020-02-09 02:29:33 +0900

    Create 11403_경로찾기.cpp

---
+++ 백준/플로이드 와샬/11403_경로찾기.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, arr[101][101];
+
+
+int main() {
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> arr[i][j];
+		}
+	}
+
+	for (int k = 0; k < n; k++) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+
+
+				if (arr[i][j] == 0) {
+					if (arr[i][k] == 1 && arr[k][j] == 1) {
+						arr[i][j] = 1;
+					}
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << arr[i][j] << " ";
+		}
+		cout << "\n";
+	}
+	return 0;
+
+}
